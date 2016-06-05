b2cf15e8faabe111d4bf8e641e72e45b2ad32055
Andrea Brancaleoni <miwaxe@gmail.com>
2016-06-05 15:47:37 +0200

    *: robust startup

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 static const char INIT_ANDROID[] = "/init.android";
-static const char INIT[] = "/usr/bin/init";
+static const char INIT[] = "/usr/bin/runit-init";
 
-static const char ROOT_FS[] = "ext4";
-static const char ROOT_PATH[] = "/data";
-static const char ROOT_DEV[] = "/dev/root";
-static const int ROOT_MAJOR = 179;
-static const int ROOT_MINOR = 49;
+static const char ROOT_FS[] = "ext2";
+static const char ROOT_PATH[] = "/newroot";
+static const char ROOT_DEV[] = "/dev/sda1";
+static const int ROOT_MAJOR = 8;
+static const int ROOT_MINOR = 1;
 
 static const char KERNEL_IMAGE[] = "/boot/Image";
-static const char KERNEL_INITRD[] = NULL;
-static const char KERNEL_CMDLINE = "root=$ROOT";
+static const char *KERNEL_INITRD = NULL;
+static const char KERNEL_CMDLINE[] = "root=$ROOT";
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <sys/mount.h>
 #include <sys/stat.h>
 #include <sys/sysmacros.h>
+#include <stdio.h>
 
 #include "config.h"
 #include "utils.h"
@@ -9,7 +10,9 @@
 #define CHECK(err) do {\
 	retval = retval | err;\
 	if (retval != 0) goto cleanup;\
-	} while (0)
+} while (0)
+
+#define FAIL(err) do { int e = err; if (e) return e; } while (0);
 
 int create_dev(unsigned int major, unsigned int minor, const char *dev) {
 	return mknod(dev, S_IFBLK | 0770, makedev(major, minor));
@@ -18,20 +21,29 @@ int create_dev(unsigned int major, unsigned int minor, const char *dev) {
 int main(int argc, char **argv, char **envp) {
 	int retval = 0;
 
-	CHECK(create_dev(ROOT_MAJOR, ROOT_MINOR, ROOT_DEV));
+	for (int i = 0, ret = 0; i < 30; i++) {
+		if (create_dev(ROOT_MAJOR, ROOT_MINOR, ROOT_DEV) &&
+				access(ROOT_DEV, R_OK)) {
+			goto cleanup;
+		} else {
+			sleep(1);
+		}
+	}
 	CHECK(mount(ROOT_DEV, ROOT_PATH, ROOT_FS, 0, ""));
 
 	// if kernel doesn't exists
+	CHECK(chdir(ROOT_PATH));
 	CHECK(chroot(ROOT_PATH));
+
 	execve(INIT, NULL, envp);
 	// else
 	//CHECK(kexec(ROOT_PATH));
 
 cleanup:
-
-	umount(ROOT_PATH);
+	chdir("/");
+	umount(ROOT_DEV);
 	remove(ROOT_DEV);
-	execve(INIT_ANDROID, argv, envp);
+	//FAIL(execve(INIT_ANDROID, argv, envp));
 
 	return retval;
 }
