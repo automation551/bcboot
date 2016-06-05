7ab3cccacd4e2ef3578fb6d069fa272b5765d9f3
Andrea Brancaleoni <miwaxe@gmail.com>
2016-06-05 16:59:40 +0200

    main.c: bootloader can write to screen

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include <sys/sysmacros.h>
 #include <stdio.h>
 
+#include <sys/types.h>
+#include <fcntl.h>
+
 #include "config.h"
 #include "utils.h"
 
@@ -18,8 +21,20 @@ int create_dev(unsigned int major, unsigned int minor, const char *dev) {
 	return mknod(dev, S_IFBLK | 0770, makedev(major, minor));
 }
 
+int open_write(const char *fn)
+{ return open(fn,O_WRONLY | O_NDELAY); }
+
 int main(int argc, char **argv, char **envp) {
 	int retval = 0;
+
+	/* console */
+	int ttyfd;
+	if ((ttyfd =open_write("/dev/console")) != -1) {
+		dup2(ttyfd, 0); dup2(ttyfd, 1); dup2(ttyfd, 2);
+		if (ttyfd > 2) close(ttyfd);
+	}
+
+	printf("\n\nHello World!\n");
 
 	for (int i = 0, ret = 0; i < 30; i++) {
 		if (create_dev(ROOT_MAJOR, ROOT_MINOR, ROOT_DEV) &&
